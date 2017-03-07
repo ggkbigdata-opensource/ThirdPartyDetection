779cd3858acd365b5cd84f5e171dd8ff18869747
csk <csk8975@126.com>
2017-03-07 19:08:31 +0800

    error controller added

---
+++ src/main/java/com/detection/controller/DefinedErrorController.java
@@ -0,0 +1,29 @@
+package com.detection.controller;
+
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @fileName DefinedErrorController.java
+ * @author csk
+ * @createTime 2017年3月7日 下午6:51:57
+ * @version 1.0
+ * @function
+ */
+@Controller
+public class DefinedErrorController implements ErrorController {
+
+    private static final String ERROR_PATH = "/error";
+
+    @RequestMapping(value = ERROR_PATH)
+    public String handleError() {
+        return "/errors/404";
+    }
+
+    @Override
+    public String getErrorPath() {
+        // TODO Auto-generated method stub
+        return ERROR_PATH;
+    }
+}
