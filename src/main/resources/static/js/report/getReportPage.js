07bff47e5476c42207e30e9159344a5f9e42a055
wilson8888 <815931551@qq.com>
2017-06-14 16:14:36 +0800

    页面提交

--- src/main/resources/static/js/report/getReportPage.js
+++ src/main/resources/static/js/report/getReportPage.js
@@ -8,19 +8,35 @@ function submitInfoData() {
     var extracteCode = $("#fetchCode").val();
     var ownerName = $("#ownerName").val();
     var dutyTel = $("#dutyTel").val();
-    console.info(extracteCode);
-    console.info(ownerName);
-    console.info(dutyTel);
+    //console.info(extracteCode);
+    //console.info(ownerName);
+    //console.info(dutyTel);
     var params = {
          'extracteCode' : extracteCode,
          'ownerName': ownerName,
          'dutyTel' : dutyTel
     }
+    if(ownerName == ''){
+    	alert('公司名称不能为空！');
+    	return;
+    }else if(dutyTel == ''){
+    	alert('电话不能为空！');
+    	return;
+    }else if(extracteCode == ''){
+    	alert('提取码不能为空！');
+    	return;
+    }
     $.getJSON(proxy, params, function(result){
-        if(result == null || 200 != result.code) {
-            alert("验证失败，输入信息有误！");
-            self.location = 'getReportPage';
-        } else {
+        if(result == null || 3 == result.code) {
+            alert("公司名称有误！");
+            //self.location = 'getReportPage';
+        }else if(result == null && result.code ==2){
+        	alert("提取码有误！");
+        	//self.location = 'getReportPage';
+        }else if(result == null && result.code ==1){
+        	alert("电话有误！");
+        	//self.location = 'getReportPage';
+        }else {
             //sessionStorage.setItem('verifyToken', result.verifyToken);
             window.location.href ="showDetailReportPage";
         }
